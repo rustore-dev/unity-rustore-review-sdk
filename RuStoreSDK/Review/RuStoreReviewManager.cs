0b32dc3fe6d01924301eac2661b7ca2f15dbbd26
e.khudenko <eugene.khudenko@gmail.com>
2023-12-19 02:04:32 +0200

    Fix SDK dependencies.

--- RuStoreSDK/Review/RuStoreReviewManager.cs
+++ RuStoreSDK/Review/RuStoreReviewManager.cs
@@ -7,7 +7,7 @@ namespace RuStore.Review {
 
     public class RuStoreReviewManager {
 
-        public static string PluginVersion = "0.1.6";
+        public static string PluginVersion = "0.1.7";
 
         private static RuStoreReviewManager _instance;
         private static bool _isInstanceInitialized;
